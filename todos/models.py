26c96c26b67dd41f1322c505f8d6c75c99302f66
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2024-09-01 18:09:55 +0300

    Add str methods and new fields to LectureTodo and Module

--- todos/models.py
+++ todos/models.py
@@ -7,9 +7,17 @@ class Module(models.Model):
     code = models.CharField(max_length=40, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        if self.code == '':
+            return self.name
+        else:
+            return f'{self.code}: {self.name}'
+
+    class Meta:
+        unique_together = ['id', 'owner']
+
 
 class BaseTodo(models.Model):
-    text = models.CharField(max_length=300)
     is_done = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,4 +28,23 @@ class BaseTodo(models.Model):
 
 class LectureTodo(BaseTodo):
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    duration = models.PositiveSmallIntegerField(blank=True)
+    topic = models.CharField(max_length=120, blank=True)
+    lecture_date = models.DateField(blank=True, null=True)
+    duration = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        if self.topic == '' and self.lecture_date is None:
+            return f'{self.module.name} | Lecture {self.id}'
+        elif self.topic == '' and self.lecture_date is not None:
+            return f'Lecture from {self.lecture_date}'
+        elif self.topic != '' and self.lecture_date is None:
+            return f'Lecture on {self.topic}'
+        else:
+            return f'{self.topic}: {self.lecture_date}'
+
+    @property
+    def as_module_and_id(self):
+        if self.module.code != '':
+            return f'{self.module.code} | Lecture {self.id}'
+        else:
+            return f'{self.module.name} | Lecture {self.id}'
